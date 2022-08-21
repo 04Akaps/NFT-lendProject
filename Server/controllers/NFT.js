7e052d302ebbff9d42ba9f52868b302ba5b2d9f8
유호진 <sdl155@jbnu.ac.kr>
2022-08-21 19:27:58 +0900

    P5js를 통한 NFT생성 및 node-html-to-img 모듈 테스트 전전

--- Server/controllers/NFT.js
+++ Server/controllers/NFT.js
@@ -1,6 +1,17 @@
 import path from "path";
+import fs from "fs";
 const __dirname = path.resolve();
 
 export const makeNFT = async (req, res) => {
-  res.sendFile(__dirname + "/NFT.html");
+  const imgHtml = __dirname + "/NFT.html";
+
+  const imgData = fs.readFileSync(imgHtml, "base64", (err, data) => {
+    return data;
+  });
+
+  console.log(imgData);
+  // npm install node-html-to-image
+  const imagePrefix = "data:image/png;base64,";
+
+  console.log(imagePrefix + imgData);
 };
