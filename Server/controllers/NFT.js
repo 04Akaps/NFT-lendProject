a9fb7857f354c9548a1c3934f1bd708e7b6242a2
유호진 <sdl155@jbnu.ac.kr>
2022-08-21 22:38:25 +0900

    p5js를 통한 image생성 및 png파일 생성 후 base64로 인코딩

--- Server/controllers/NFT.js
+++ Server/controllers/NFT.js
@@ -1,17 +1,32 @@
 import path from "path";
 import fs from "fs";
+import nodeHtmlToImage from "node-html-to-image";
+
 const __dirname = path.resolve();
 
 export const makeNFT = async (req, res) => {
   const imgHtml = __dirname + "/NFT.html";
 
-  const imgData = fs.readFileSync(imgHtml, "base64", (err, data) => {
+  const htmlData = await fs.readFileSync(imgHtml, "utf8", (err, data) => {
     return data;
   });
 
-  console.log(imgData);
-  // npm install node-html-to-image
+  await nodeHtmlToImage({
+    output: "./newNFT.png",
+    html: htmlData,
+  });
+
+  const newNFTImageLink = __dirname + "/newNFT.png";
+
+  const newNFTImageData = await fs.readFileSync(
+    newNFTImageLink,
+    "base64",
+    (err, data) => {
+      return data;
+    }
+  );
+
   const imagePrefix = "data:image/png;base64,";
 
-  console.log(imagePrefix + imgData);
+  console.log(imagePrefix + newNFTImageData);
 };
