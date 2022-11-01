8bdc6e5d26849fb112bbc0b1785b6d4def3d28e3
hojin <sdl155@naver.com>
2022-11-01 16:43:55 +0900

    contract 모두 삭제 및 다시 구상하여 진행 예정 일단 Front 작업 우선 진행

--- truffle/contracts/interface/IKIP37/IKIP37MetadataURI.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
-
-import "./IKIP37.sol";
-
-interface IKIP37MetadataURI is IKIP37 {
-    function uri(uint256 id) external view returns (string memory);
-}
