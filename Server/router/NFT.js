735fa8099ff529ea65f15140b89ed3fa24b4772b
유호진 <sdl155@jbnu.ac.kr>
2022-11-13 11:58:26 +0900

    Swagger 일부 적용 및 테스트

--- Server/router/NFT.js
+++ Server/router/NFT.js
@@ -5,7 +5,77 @@ const router = express.Router();
 
 router.post("/test", makeNFT);
 // 위에 router는 후에 제거하여 eventListening으로 활용
+
+/**
+ * @swagger
+ * paths:
+ *  /getNFT/{id}:
+ *   get:
+ *    summary : "get NFT Metadata"
+ *    parameters:
+ *       - in: query
+ *         name: id
+ *         type: integer
+ *         required: true
+ *    description : "NFT Metadata 조회"
+ *    tags: [NFT]
+ *    responses:
+ *      "200":
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example:
+ *                   "Metadata"
+ *      "201":
+ *        content:
+ *          application/json:
+ *            schema:
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example:
+ *                   "Not Existed"
+ */
 router.get("/getNFT/:id", getNFTMetaData);
+
+/**
+ * @swagger
+ * paths:
+ *  /getNFTImage/{id}:
+ *   get:
+ *    summary : "get NFT Image"
+ *    description : "NFT Image 조회"
+ *    parameters:
+ *       - in: query
+ *         name: id
+ *         type: integer
+ *         required: true
+ *    tags: [NFT]
+ *    responses:
+ *      "200":
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example:
+ *                   "img"
+ *      "201":
+ *        content:
+ *          application/json:
+ *            schema:
+ *              properties:
+ *                message:
+ *                  type: string
+ *                  example:
+ *                   "Not Existed"
+ */
 router.get("/getNFTImage/:id", getNFTImage);
 
 export default router;
