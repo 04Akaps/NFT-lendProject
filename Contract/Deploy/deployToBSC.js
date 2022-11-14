beb51e7db82b54bb125d8410f31b2794abd457d9
hojin <sdl155@naver.com>
2022-11-14 13:03:57 +0900

    deploy.js를 통해서 배포 및 script를 작성하여 setting 자동화 -> Contract 작업 종료 만약 에러 있을시에 수정 예정

---
+++ Contract/Deploy/deployToBSC.js
@@ -0,0 +1,14 @@
+const shell = require("shelljs");
+const cliString = "npx hardhat run --network bscTest ../scripts/deploy.js";
+const settingString = "npm run bsc";
+const init = async () => {
+  await shell.exec(cliString);
+
+  console.log("");
+  console.log("배포 완료");
+  console.log("");
+
+  await shell.exec(settingString);
+};
+
+init();
