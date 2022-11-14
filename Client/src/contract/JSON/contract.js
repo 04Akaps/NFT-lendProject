801a9e5d5c4e538135392a295d699fbd0ac67050
hojin <sdl155@naver.com>
2022-11-14 13:37:34 +0900

    Contract Instance 작업 완료

---
+++ Client/src/contract/JSON/contract.js
@@ -0,0 +1,37 @@
+import Web3 from "web3";
+export const web3 = new Web3(window.ethereum);
+
+const zolCoreJson = require("./zolCore.json");
+const zolItemJson = reuqire("./zolItem.json");
+const zolMiningPoolJson = requre("./zolMiningPool.json");
+const zolNFTJson = require("./zolNFT.json");
+const zolTokenJson = require("./zolToken.json");
+
+export const zolCoreABI = zolCoreJson.abi;
+export const zolCoreAddress = zolCoreJson.address;
+
+export const zolItemABI = zolItemJson.abi;
+export const zolItemAddress = zolItemJson.address;
+
+export const zolMiningPoolABI = zolMiningPoolJson.abi;
+export const zolMiningPoolAddress = zolMiningPoolJson.address;
+
+export const zolNFTABI = zolNFTJson.abi;
+export const zolNFTAddress = zolNFTJson.address;
+
+export const zolTokenABI = zolTokenJson.abi;
+export const zolTokenAdress = zolTokenJson.address;
+
+export const zolCoreInstance = new web3.eth.Contract(zolCoreABI, zolCoreAddress)
+  .methods;
+
+export const zolNFTInstance = new web3.eth.Contract(zolNFTABI, zolNFTAddress)
+  .methods;
+
+export const zolItemInstance = new web3.eth.Contract(zolItemABI, zolItemAddress)
+  .methods;
+
+export const zolTokenInstance = new web3.eth.Contract(
+  zolTokenABI,
+  zolTokenAdress
+).methods;
